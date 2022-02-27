9b8f96f39a00c79a9bf413ee8bc847a658812b56
Vincent Picard <vpicard-entreprise@outlook.com>
2022-02-27 23:00:57 +0100

    Addition of Nav between Experience et Education. I must make Education active when page load.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio"
 import { Routes, Route } from "react-router-dom";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="App">
         <Routes>
           <Route path='' exact element={<Home/>} />
-          <Route path='about' exact element={<About/>} />
-          <Route path='contact' exact element={<Contact/>}/>
-          <Route path='portfolio' exact element={<Portfolio/>}/>
+          <Route path='/about' exact element={<About/>}>
+            <Route path="/about/education" exact element={<Education/>}/>
+            <Route path="/about/experience" exact element={<Experience/>}/>
+          </Route>
+          <Route path='/contact' exact element={<Contact/>}/>
+          <Route path='/portfolio' exact element={<Portfolio/>}/>
         </Routes>
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import { Outlet, Link } from 'react-router-dom';
 import Navigation from './Navigation';
 
 const About = () => {
     return (
-        <div>
+        <div className='about'>
             <Navigation />
+            <Row>
+                <Col lg={12}>
+                    <img alt="Profile" src={require("../assets/images/main-profile-picture.png")} />
+                </Col>
+            </Row>
+            <Row className='w-100'>
+                <Col lg={12} className="mt-3 mb-2 w-100">
+                    <nav>
+                        <Link className='nav-link' to="/about/education" >Education</Link>
+                        <Link className='nav-link' to="/about/experience" >Experience</Link>
+                    </nav>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg={12}>
+                    <Outlet/>
+                </Col>
+            </Row>
         </div>
     );
 };
